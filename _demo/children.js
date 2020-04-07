35c8015066060a490c78a1d0aa7c19f69ab8ba26
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-07 07:20:29 +0000

    Deploy ufologist/page-schema to github.com/ufologist/page-schema.git:gh-pages

--- _demo/children.js
+++ _demo/children.js
@@ -4,21 +4,36 @@
   "title": "自定义组件示例",
   "body": {
     "type": "form",
+    "mode": "horizontal",
+    "initApi": "https://houtai.baidu.com/api/mock2/page/initData",
     "controls": [{
-      "type": "text",
-      "label": "用户名",
-      "name": "usename"
+      "type": "static",
+      "label": "日期",
+      "name": "date"
     }, {
-      "name": "a",
+      "name": "title",
       "children": function children(_ref) {
         var value = _ref.value,
             onChange = _ref.onChange;
-        return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, "\u8FD9\u4E2A\u662F\u4E2A\u81EA\u5B9A\u4E49\u7EC4\u4EF6"), /*#__PURE__*/React.createElement("p", null, "\u5F53\u524D\u503C\uFF1A", value), /*#__PURE__*/React.createElement("a", {
-          className: "btn btn-default",
-          onClick: function onClick() {
-            return onChange(Math.round(Math.random() * 10000));
+        return /*#__PURE__*/React.createElement("div", {
+          "class": "a-Form-item a-Form-item--horizontal"
+        }, /*#__PURE__*/React.createElement("label", {
+          "class": "a-Form-label a-Form-itemColumn--2"
+        }, "\u5B57\u6BB5"), /*#__PURE__*/React.createElement("div", {
+          "class": "a-Form-value"
+        }, /*#__PURE__*/React.createElement("div", {
+          "class": "form-control-static"
+        }, /*#__PURE__*/React.createElement("div", {
+          "class": "a-Form-control"
+        }, /*#__PURE__*/React.createElement("span", {
+          "class": "a-TplField"
+        }, value), /*#__PURE__*/React.createElement("a", {
+          className: "btn btn-link btn-xs",
+          onClick: function onClick(event) {
+            event.target.parentNode.removeChild(event.target);
+            onChange(Math.round(Math.random() * 10000));
           }
-        }, "\u968F\u673A\u4FEE\u6539"));
+        }, "\u67E5\u770B")))));
       }
     }]
   }
